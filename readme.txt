df641b288342291f2be2aaaf5374d7ec6dcaf004
Jason Coleman <jason@strangerstudios.com>
2017-02-12 10:08:41 -0500

    Adding stylesheet and plugin repo images; readme update

--- readme.txt
+++ readme.txt
@@ -7,27 +7,27 @@ Stable tag: 1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Shows a list of a given page’s subpages, with optional excerpt, link, button, and images.
+Dynamic content organization with flexible display options.
 
 == Description ==
 
-Enter the shortcode [subpagelist] in any page or post to generate the list. An example shortcode an all shortcode attributes are displayed below:
+Shows a list of a subpages, with display options for content or excerpts, buttons and links, featured images, and layout. Enter the shortcode [subpagelist] in any page to generate the list. An example shortcode and all shortcode attributes are displayed below:
 
 = Example Subpagelist Shortcode =
 [subpagelist exclude="3,5" layout="3col" link="true" orderby="title" order="ASC" show="excerpt" thumbnail="thumbnail"]
 
 = Shortcode Attributes Include: =
-<ul>
-	<li>exclude: A comma-separated list of the page IDs to exclude from display. (default: none)</li>
-	<li>layout: Set this attribute if you would like to list the pages in columns; accepts "2col", "3col" or "4col". (default: 1 column)</li>
-	<li>link: Hyperlink the page title; accepts "true" or "false". (default: true)</li>
-	<li>orderby: Accepts any orderby parameter as <a href="http://codex.wordpress.org/Class_Reference/WP_Query#Order_.26_Orderby_Parameters">defined in the codex</a>. (default: menu_order)</li>
-	<li>order: Accepts ASC or DESC as <a href="http://codex.wordpress.org/Class_Reference/WP_Query#Order_.26_Orderby_Parameters">defined in the codex</a>. (default: ASC)</li>
-	<li>post_parent: Override the parent page ID to pull in a different list of subpages; accepts any page ID. (default: current page ID)</li>
-	<li>show: Display "excerpt", "content", or "none". (default: excerpt)</li>
-	<li>show_children: Optionally display an unordered list of the child page's children; accepts "true" or "false". (default: none)</li>
-	<li>thumbnail: Optionally hide or show the subpage's featured image; accepts "thumbnail", "medium", "large" or "false". (default: false)</li>
-</ul>
+* exclude: A comma-separated list of the page IDs to exclude from display. (default: none)
+* layout: Set this attribute if you would like to list the pages in columns; accepts "2col", "3col" or "4col". (default: 1 column)
+* link: Hyperlink the page title; accepts "true" or "false". (default: true)
+* link_text: The "more link" text to display. Accepts "your custom text". (default: "(more...)")
+* orderby: Accepts any orderby parameter as [defined in the codex](https://developer.wordpress.org/reference/classes/wp_query/#order-orderby-parameters). (default: menu_order)
+* order: Accepts ASC or DESC as [defined in the codex](https://developer.wordpress.org/reference/classes/wp_query/#order-orderby-parameters). (default: ASC)
+* post_parent: Override the parent page ID to pull in a different list of subpages; accepts any page ID. (default: current page ID)
+* show: Display "excerpt", "content", or "none". (default: excerpt)
+* show_children: Optionally display an unordered list of the child page's children; accepts "true" or "false". (default: false)
+* thumbnail: Optionally show the subpage's featured image; accepts "thumbnail", "medium", "large" or "false". (default: false)
+* thumbnail_align: Optionally force the alignment of the featured image. Leave blank to inherit the suggest alignment for the given layout; accepts "center", "left", "right", "none". (default: false)
 
 == Installation ==
 
@@ -40,17 +40,19 @@ In your WordPress admin, go to Plugins > Add New to install Subpagelister, or:
 4. Activate the plugin through the 'Plugins' menu in WordPress.
 
 = Start Using the Shortcodes =
-Browse the Subpagelister documentation to see all shortcode attributes, and to view sample shortcode demos.
+Browse the Subpagelister documentation to see all shortcode attributes and to view sample shortcode demos.
 
 [View Documentation](http://strangerstudios.com/plugins/subpagelister/)
 
 == Screenshots ==
-1. Screen
- 
+1. Sample shortcode output of [subpagelist link="button" link_text="Keep Reading" orderby="title" show="excerpt" thumbnail="medium" thumbnail_align="left"]
+2. Sample shortcode output of [subpagelist layout="3col" link="button" link_text="Keep Reading" orderby="title" show="none" thumbnail="medium"]
+3. Sample shortcode output of [subpagelist layout="2col" orderby="title" show="excerpt" thumbnail="true"]
+
 == Frequently Asked Questions ==
 
 = Where can I find Subpagelister documentation? =
-For help setting up and configuring the Memberlite Shortcodes plugin, please refer to [documentation](http://strangerstudios.com/plugins/subpagelister/).
+For help setting up and configuring the Subpagelister plugin, please refer to [documentation](http://strangerstudios.com/plugins/subpagelister/).
 
 == Changelog ==
 
--- subpagelister.php
+++ subpagelister.php
@@ -2,163 +2,178 @@
 /*
 Plugin Name: Subpagelister
 Plugin URI: http://www.strangerstudios.com/plugins/subpagelister/
-Description: Shows a list of a given page’s subpages, with optional excerpt, link, button, and images.
+Description: Dynamic content organization with flexible display options.
 Version: 1.0
 Author: kimannwall, strangerstudios
 Author URI: http://www.strangerstudios.com
 */
 
-define('SUBPAGELISTER_DIR', dirname(__FILE__) );
-define('SUBPAGELISTER_URL', plugins_url('', __FILE__));
-define('SUBPAGELISTER_VERSION', '1.0.1');
+define( 'SUBPAGELISTER_DIR', dirname(__FILE__) );
+define( 'SUBPAGELISTER_URL', plugins_url( '', __FILE__ ) );
+define( 'SUBPAGELISTER_VERSION', '1.0' );
+
+/*
+	Enqueue Stylesheet
+*/
+function subpagelister_init_styles()
+{	
+	wp_enqueue_style( 'subpagelister_frontend', SUBPAGELISTER_URL . '/css/subpagelister.css', array(), SUBPAGELISTER_VERSION);	
+}
+add_action( 'wp_enqueue_scripts', 'subpagelister_init_styles' );	
 
 /**
  * Shows the subpages (in page order) and excerpts.
  *
  * @param array $args Configuration arguments.
  */
-function subpagelister_shortcode_handler($atts, $content=null, $code="") {
+function subpagelister_shortcode_handler( $atts, $content=null, $code='' ) {
 	global $post;
 	
 	// $atts    ::= array of attributes
 	// $content ::= text within enclosing form of shortcode element
 	// $code    ::= the shortcode found, when == callback name
-	// examples: [subpagelist exclude="1,2,3" show="excerpt" link="true" orderby="menu_order"]
+	// examples: [subpagelist exclude="1,2,3" show="excerpt" link="button" orderby="menu_order"]
 	
-	extract(shortcode_atts(array(
-		'exclude' => NULL,
-		'layout' => NULL,
-		'link' => true,
-		'link_text' => '(more...)',
-		'orderby' => 'menu_order',
-		'order' => 'ASC',
-		'post_parent' => $post->ID,
-		'show' => 'excerpt',
-		'show_children' => false,
-		'thumbnail' => false,
-	), $atts));
+	extract( shortcode_atts ( array (
+		'exclude'			=> NULL,
+		'layout'				=> NULL,
+		'link'				=> true,
+		'link_text'			=> '(more...)',
+		'orderby'			=> 'menu_order',
+		'order'				=> 'ASC',
+		'post_parent'		=> $post->ID,
+		'show'				=> 'excerpt',
+		'show_children'		=> false,
+		'thumbnail'			=> false,
+		'thumbnail_align'	=> false,
+	), $atts ) );
 	
-	if($link && strtolower($link) != "false")
-		$link = true;
-	else
+	if ( empty($link) || strtolower( $link ) === 'false' ) {
 		$link = false;
-
-	if($show_children == "0" || $show_children == "false" || $show_children == "no")
-		$show_children = false;
-				
-	if($thumbnail && strtolower($thumbnail) != "false")
-	{
-		/*if(strtolower($thumbnail) == "icon")
-			$thumbnail = "icon";
-		if(strtolower($thumbnail) == "medium")
-			$thumbnail = "medium";
-		elseif(strtolower($thumbnail) == "large")
-			$thumbnail = "large";
-		else
-			$thumbnail = "thumbnail";
-		*/
 	}
-	else
+	
+	if ( $show_children === '0' || strtolower( $show_children ) === 'false' || strtolower( $show_children ) === 'no' ) {
+		$show_children = false;
+	}
+				
+	if ( empty($thumbnail) || strtolower( $thumbnail ) === 'false' ) {
 		$thumbnail = false;
-
+	} elseif ( strtolower( $thumbnail ) === 'true' ) {
+		$thumbnail = 'thumbnail';
+	}
+	
+	if( !empty( $thumbnail_align ) ) {
+		if( strtolower( $thumbnail_align ) === 'center' ) {
+			$thumbnail_align = 'aligncenter';
+		} elseif( strtolower( $thumbnail_align ) === 'left' ) {
+			$thumbnail_align = 'alignleft';
+		} else {
+			$thumbnail_align = 'alignright';
+		}
+	}
+	
 	// prep exclude array
-	$exclude = str_replace(" ", "", $exclude);
-	$exclude_array = explode(",", $exclude);
+	$exclude = str_replace( ' ', '', $exclude );
+	$exclude_array = explode( ',', $exclude );
 		
 	// our return string
-	$r = "";
-		
-	// get posts
-	$args = array(
-		"post_type"=>"page",
-		"showposts"=>-1,
-		"orderby"=>$orderby,
-		"post_parent"=>$post_parent,
-		"order"=>$order,
-		"post__not_in"=>$exclude_array
-	);
-	$subpageposts = get_posts($args);
-	
-	$layout_cols = preg_replace('/[^0-9]/', '', $layout);;	
-	if(!empty($layout_cols))
-		$subpageposts_chunks = array_chunk($subpageposts, $layout_cols);
-	else
-		$subpageposts_chunks = array_chunk($subpageposts, '1');
+	$r = '';
+	$r = '<div class="subpagelist">';
 
-  	//to show excerpts. save the old value to revert
+	// get posts
+	$args = array (
+		'post_type'		=> 'page',
+		'showposts'		=> -1,
+		'orderby'		=> $orderby,
+		'post_parent'	=> $post_parent,
+		'order'			=> $order,
+		'post__not_in'	=> $exclude_array
+	);
+	$subpageposts = get_posts( $args );
+	
+	$layout_cols = preg_replace( '/[^0-9]/', '', $layout );	
+	if ( !empty ( $layout_cols ) ) {
+		$subpageposts_chunks = array_chunk( $subpageposts, $layout_cols );
+	} else {
+		$subpageposts_chunks = array_chunk( $subpageposts, '1' );
+	}
+
+  	//to show excerpts or full content. save the old value to revert
 	global $more;
 	$oldmore = $more;
-	$more = 0;
 
 	// the Loop
-	$nchunks = count($subpageposts_chunks);		
-	for($i = 0; $i < $nchunks; $i++):
+	$nchunks = count( $subpageposts_chunks );		
+	for( $i = 0; $i < $nchunks; $i++ ):
 		$row = $subpageposts_chunks[$i];
 		$r .= '<div class="row">';
-		foreach($row as $post): 
-			setup_postdata($post);
+		foreach( $row as $post ):  
+			setup_postdata( $post );
 			$r .= '<div class="medium-';			
-			if($layout == '2col')
+			if ( $layout === '2col' ) {
 				$r .= '6';
-			elseif($layout == '3col')
+			} elseif ( $layout === '3col' ) {
 				$r .= '4';
-			elseif($layout == '4col')
+			} elseif ( $layout === '4col' ) {
 				$r .= '3';
-			else
+			} else {
 				$r .= '12';
+			}
 			$r .= ' columns">';
-			$r .= '<article id="post-' . get_the_ID() . '" class="' . implode(" ", get_post_class()) . ' subpagelist_item">';
+			$r .= '<article id="post-' . get_the_ID() . '" class="' . implode( " ", get_post_class() ) . ' subpagelist_item">';
 		
-			if ( has_post_thumbnail() && empty($layout) && !empty($thumbnail) && $thumbnail !== "icon")
-			{
-				if($layout == '3col' || $layout == '4col')
-					$thumbnail_class = "aligncenter";
-				else
-					$thumbnail_class = "alignright";	
-				if($link)
-					$r .= '<a href="' . get_permalink() . '">' . get_the_post_thumbnail($post->ID, $thumbnail, array('class' => $thumbnail_class )) . '</a>';
-				else
-					$r .= get_the_post_thumbnail($post->ID, $thumbnail, array('class' => $thumbnail_class) );
+			if ( has_post_thumbnail() && empty( $layout ) && !empty( $thumbnail ) ) {
+				if( empty( $thumbnail_align ) ) {
+					$thumbnail_align = "alignright";	
+				}
+				if ( $link) {
+					$r .= '<a href="' . get_permalink() . '">' . get_the_post_thumbnail( $post->ID, $thumbnail, array('class' => $thumbnail_align ) ) . '</a>';
+				} else {
+					$r .= get_the_post_thumbnail( $post->ID, $thumbnail, array('class' => $thumbnail_align) );
+				}
 			}
 			
 			$r .= '<header class="entry-header">';
 			$r .= '<h1 class="entry-title">';
 	
-			if($link)
-			{
+			if ( $link ) {
 				$r .= '<a href="' . get_permalink() . '" rel="bookmark">';
 				$r .= the_title('','',false);
 				$r .= '</a>';
-			}
-			else
-			{
-				$r .= the_title('','',false);
+			} else {
+				$r .= the_title( '', '', false );
 			}
 						
 			$r .= '</h1>';
 			$r .= '</header>';		
 			$r .= '<div class="entry-content">';		
 	
-			if ( has_post_thumbnail() && !empty($layout) && !empty($thumbnail) && $thumbnail !== "icon") 
-			{			
-				if($layout == '3col' || $layout == '4col')
-					$thumbnail_class = "aligncenter";
-				else
-					$thumbnail_class = "alignright";		
-				if($link)
-					$r .= '<a href="' . get_permalink() . '">' . get_the_post_thumbnail($post->ID, $thumbnail, array('class' => $thumbnail_class )) . '</a>';
-				else
-					$r .= get_the_post_thumbnail($post->ID, $thumbnail, array('class' => $thumbnail_class) );
+			if ( has_post_thumbnail() && !empty( $layout ) && !empty( $thumbnail ) ) {	
+				if( empty( $thumbnail_align ) ) {							
+					if ( $layout === '3col' || $layout === '4col' ) {
+						$thumbnail_align = "aligncenter";
+					} else {
+						$thumbnail_align = "alignright";
+					}
+				}
+				if ( $link ) {
+					$r .= '<a href="' . get_permalink() . '">' . get_the_post_thumbnail( $post->ID, $thumbnail, array('class' => $thumbnail_align ) ) . '</a>';
+				} else {
+					$r .= get_the_post_thumbnail( $post->ID, $thumbnail, array( 'class' => $thumbnail_align ) );
+				}
 			}
 										
-			if($show == "excerpt")
-				$r .= apply_filters('the_content', preg_replace("/\[subpagelist[^\]]*\]/", "", get_the_excerpt( '' )));
-			elseif($show == "content")
-				$r .= apply_filters('the_content', preg_replace("/\[subpagelist[^\]]*\]/", "", get_the_content( '' )));						
-			else
+			if ( $show === "excerpt" ) {
+				$more = 0;
+				$r .= apply_filters('the_content', preg_replace("/\[subpagelist[^\]]*\]/", "", get_the_excerpt( '' ) ) );
+			} elseif ( $show === "content" ) {
+				$more = 1;
+				$r .= apply_filters('the_content', preg_replace("/\[subpagelist[^\]]*\]/", "", get_the_content( '' ) ) );					
+			} else {
 				$r .= '';
-				
-			if(!empty($show_children))
+			}
+			
+			if ( !empty( $show_children ) )
 			{
 				$r .= '<ul class="subpagelist_children">';
 				$r .= '<li class="page_item page-item-' . $post->ID . '"><a href="' . get_permalink() . '" rel="bookmark">' . the_title('','',false) . '</a></li>';
@@ -166,11 +181,18 @@ function subpagelister_shortcode_handler($atts, $content=null, $code="") {
 				$r .= '</ul>';			
 			}
 			
-			if($link)
+			if ( $link )
 			{
-				$r .= '<a class="more-link" href="' . get_permalink() . '" rel="bookmark">';
+				$r .= '<p><a href="' . get_permalink() . '" rel="bookmark" class="more-link';
+				if ( $link === "button" ) {
+					$r .= ' subpagelist_btn';
+				}
+				if ( $layout === '3col' || $layout === '4col' ) {
+					$r .= ' subpagelist_btn-block';
+				}
+				$r .= '">';
 				$r .= $link_text;
-				$r .= '</a>';
+				$r .= '</a></p>';
 			}
 								
 			$r .= '</div>'; //end entry-content
@@ -180,10 +202,13 @@ function subpagelister_shortcode_handler($atts, $content=null, $code="") {
 			endforeach;
 		$r .= '</div>'; //end row
 		
-		if($i < $nchunks - 1)
+		if ( $i < $nchunks - 1 ) {
 			$r .= "<hr />";
+		}
 		
 	endfor;
+
+	$r .= '</div>'; // end subpagelist
 
 	//Reset Query
 	wp_reset_query();
@@ -193,12 +218,11 @@ function subpagelister_shortcode_handler($atts, $content=null, $code="") {
 	
 	return $r;
 }
-remove_shortcode('memberlite_subpagelist');	//replace shortcode bundled with Memberlite 2.0 and prior or anywhere else
-add_shortcode('memberlite_subpagelist', 'subpagelister_shortcode_handler');
+add_shortcode( 'subpagelist', 'subpagelister_shortcode_handler' );
 
-function subpagelister_remove_subpagelist_from_excerpt($excerpt)
+function subpagelister_remove_subpagelist_from_excerpt( $excerpt )
 {
-	$excerpt = preg_replace("/\[subpagelist[^\]]*\]/", "", $excerpt);
+	$excerpt = preg_replace( "/\[subpagelist[^\]]*\]/", "", $excerpt );
 	return $excerpt;
 }
-add_filter("the_excerpt", "subpagelister_remove_subpagelist_from_excerpt");
+add_filter( 'the_excerpt', 'subpagelister_remove_subpagelist_from_excerpt' );
